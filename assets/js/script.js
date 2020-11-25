55398691891f6b198110646079be2cb970ab2d8a
Dwi Wahyu <madecahya72@gmail.com>
2020-11-25 17:21:24 +0800

    adding sticky navbar

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,7 +1,20 @@
+// Menu Toggle for Mobile
 const menu = document.getElementById("menu-toggle");
 const nav = document.getElementById("nav-links");
+const navButton = document.getElementById("menu-toggle span");
 
 menu.addEventListener("click", function () {
   nav.classList.toggle('show');
 })
 
+// Sticky Navbar
+// Add .sticky class when scrolled
+var navbar = document.getElementById("header");
+
+window.onscroll = function () {
+  if (window.pageYOffset > 10) {
+    navbar.classList.add("sticky")
+  } else {
+    navbar.classList.remove("sticky");
+  }
+};
