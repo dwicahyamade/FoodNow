9ba2e20e99f9dc7299e2cb64d2647763a4bb9dc8
Dwi Wahyu <madecahya72@gmail.com>
2020-11-26 12:28:56 +0800

    adding text animation

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,10 +1,10 @@
 // Menu Toggle for Mobile
 const menu = document.getElementById("menu-toggle");
 const nav = document.getElementById("nav-links");
-const navButton = document.getElementById("menu-toggle span");
 
 menu.addEventListener("click", function () {
   nav.classList.toggle('show');
+  menu.classList.toggle('click');
 })
 
 // Sticky Navbar
@@ -29,4 +29,61 @@ $('.page-scroll').on('click', function (e) {
   }, 800);
 
   e.preventDefault();
+})
+
+// Back to top
+$(window).scroll(function () {
+  if ($(this).scrollTop() > 100) {
+    $('.back-to-top').fadeIn('slow');
+  } else {
+    $('.back-to-top').fadeOut('slow');
+  }
+});
+
+$('.back-to-top').click(function () {
+  $('html, body').animate({
+    scrollTop: 0
+  }, 800);
+});
+
+// Hero text animation
+$(window).on('load', function () {
+  setTimeout(function () {
+    $('.hero-text').addClass('show');
+  }, 500);
+});
+
+// About text animation
+$(window).scroll(function () {
+  var wScroll = $(this).scrollTop();
+  if (wScroll > $('#About').offset().top - 450) {
+    setTimeout(function () {
+      $('.about-text').addClass('show');
+    }, 300);
+    setTimeout(function () {
+      $('.about-img-content').addClass('show');
+    }, 300);
+  }
+})
+
+// Feature text animation
+$(window).scroll(function () {
+  var wScroll = $(this).scrollTop();
+  if (wScroll > $('#Features').offset().top - 900) {
+    $('.container div').each(function (i) {
+      setTimeout(function () {
+        $('.container div').eq(i).addClass('show');
+      }, 300 * (i + 1));
+    });
+  }
+})
+
+// How it's text animation
+$(window).scroll(function () {
+  var wScroll = $(this).scrollTop();
+  if (wScroll > $('#How').offset().top) {
+    setTimeout(function () {
+      $('.how-text').addClass('show');
+    }, 300);
+  }
 })
